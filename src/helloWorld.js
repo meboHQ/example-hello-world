80d640e0a96c03a61fc7caf7aa3e5034e734fd87
Paulo Nogueira <paulondc@gmail.com>
2018-08-15 23:50:36 -0700

    Update helloWorld.js

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -11,9 +11,9 @@ class MyAction extends Mebo.Action{
     this.createInput('repeat: numeric', {defaultValue: 10});
   }
 
-  _perform(data){
+  async _perform(data){
     const result = data.message.repeat(data.repeat);
-    return Promise.resolve(result);
+    return result;
   }
 }
 
